2f636e7f04c52037eaf40bda259baf18de4a82a6
hmwillars10 <betowillars@icloud.com>
2020-05-11 16:57:52 -0600

    commit inicial

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoiamhtd2lsbGFycyIsImEiOiJja2EydW1mMWIwOG81M2V0YjNuaDZwcmdsIn0.bk6lHmzR3zuGMg8LADBNlw';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center:[-111.666069,26.044445],
+zoom:6
+})
